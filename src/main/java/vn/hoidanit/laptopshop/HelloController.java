afd3a8f04b55229a5dc9ad42d43d6e793e44f767
Thanh3423 <nguyenvanthnahtk05@mail.com>
2024-08-06 20:24:31 +0700

    connect mysql

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World Thanh ";
+        return "Hello World Thanh 123 ";
     }
 
+    @GetMapping("/user")
+    public String userpage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminpage() {
+        return "Only admin can access this page ";
+    }
 }
